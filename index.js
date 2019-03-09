86e4b94017f9544598c6601bc4b6d58f5cf9b9f8
joe-code-net <joe.code.netgmail.com>
2019-03-08 20:23:42 -0700

    Internal Modules

--- index.js
+++ index.js
@@ -3,3 +3,15 @@
  * Comience aqui
  * *************
  */
+//Call Modules
+ const math = require("./math");
+const greet = require("./greetings/"); //call index.js
+const hello = require("./greetings/hello");
+
+//Use Functions from Modules
+console.log(math.add(4, 5));
+console.log(math.divide(4, 5));
+console.log(math.multiply(4, 5));
+console.log(math.substract(4, 5));
+console.log(greet.greet("Joe"));
+console.log(hello.sayHello("Carlos"));
