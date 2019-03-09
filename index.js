736d1890f80e355579cb7aa6b62be707461470a4
joe-code-net <joe.code.netgmail.com>
2019-03-08 22:51:10 -0700

    Sync - Async

--- index.js
+++ index.js
@@ -5,3 +5,11 @@
  * *************
  * 
  */
+const add = require("./src/addition");
+const sim = require("./src/intensive");
+
+//add.syncSum(1,2);
+//add.asyncSum(2,4);
+
+//sim.simulateSync();
+sim.simulateAsync();
