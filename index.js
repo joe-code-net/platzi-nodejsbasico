ff73cc3268c76890097d36cae91d61b01c568900
joe-code-net <joe.code.netgmail.com>
2019-03-12 20:48:02 -0600

    Async/Await

--- index.js
+++ index.js
@@ -5,3 +5,21 @@
  * *************
  * 
  */
+const call = require("./src/call");
+
+//call.greet("Joe");
+//call.withPromise("Joe", "Net")
+//        .then(n=>console.log(n));
+
+async function callWithPromise() {
+    const fullName0 = await call.withPromise("Joe", "Net")
+    console.log(fullName0);
+
+    fullName1 = call.withPromise("Carlos", "Net")
+    console.log(fullName1);
+
+    fullName2 = await call.withPromise("Emir", "Net")
+    console.log(fullName2);
+}
+
+callWithPromise();
