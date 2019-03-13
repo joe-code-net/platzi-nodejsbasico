6614ab3f299fa862335462c3eb9abf1cca23396d
joe-code-net <joe.code.netgmail.com>
2019-03-12 22:24:40 -0600

    Filesystem

--- index.js
+++ index.js
@@ -5,3 +5,19 @@
  * *************
  * 
  */
+const fs = require("fs");
+const ops = require("./src/fileops");
+
+let incValue;
+
+fs.readFile("./resources/number.txt", "utf8", (err, text)=>{
+    if (err) throw err;
+    const numbers = text.split("\n").map(n=>Number(n));
+    console.log(numbers);
+    incValue = ops.incrementValues(numbers);
+    console.log(incValue);
+    fs.writeFile("./resources/numbernew.txt", incValue.join("\n"), (err, result)=>{
+        if(err) throw err;
+    });
+})
+
