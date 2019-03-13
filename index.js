674cd99c20a88fa7f51c9b001b3620c1795e1e49
joe-code-net <joe.code.netgmail.com>
2019-03-12 20:07:04 -0600

    Callback - Promesas

--- index.js
+++ index.js
@@ -5,3 +5,11 @@
  * *************
  * 
  */
+ const call = require("./src/call");
+
+//console.log(call.sync("Joe"));
+//call.withCallback("Joe", call.sync);
+
+//console.log(call.withPromise("Joe"));
+call.withPromise("Joe").then(name => { console.log(name) });
+
