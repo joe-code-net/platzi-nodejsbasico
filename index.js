df38ea248123879686dcf75030927e3f2432bb8f
joe-code-net <joe.code.netgmail.com>
2019-03-24 14:54:34 -0700

    Errors

--- index.js
+++ index.js
@@ -5,3 +5,18 @@
  * *************
  * 
  */
+const errors = require("./src/errors");
+
+// try {
+//     errors.standardErr.range();
+// } catch(err) {
+//     console.log("Ha ocurrido un error: ", err);
+// }
+ 
+// console.log("Hola");
+
+const error = require("./src/errors");
+const handle = require("./src/handling");
+
+handle.errorFirstCallbackWrong();
+
