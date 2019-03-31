b7cba87679dbb9cf0bb5d546f62dae9e3be231e6
joe-code-net <joe.code.netgmail.com>
2019-03-30 18:21:09 -0700

    Events

--- index.js
+++ index.js
@@ -5,3 +5,10 @@
  * *************
  * 
  */
+const greet = require("./src/greet");
+
+greet.emit("clap");
+greet.emit("shout", "Hello");
+greet.emit("call", "Joe", (name) => {
+    console.log("Estamos llamdo a: ", name);
+})
