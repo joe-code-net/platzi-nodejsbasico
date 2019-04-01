fb971a5a81b0bf864777e82ceaf737f14ddaf363
joe-code-net <joe.code.netgmail.com>
2019-03-31 21:17:13 -0700

    Express

--- index.js
+++ index.js
@@ -5,3 +5,22 @@
  * *************
  * 
  */
+
+ const express = require('express');
+ const app = express();
+
+ app.get("/app", (req, res)=>{
+    res.send("Estoy en la ruta /app");
+ });
+ 
+ app.get("/", (req, res)=>{
+    res.send("Estoy e la ruta HOME");
+ });
+
+//Comodin cual peticion que no encaje llega a esta
+ app.get("*", (req, res)=>{
+    res.send("No se donde estoy");
+ });
+ app.listen(3000, ()=>{
+    console.log("El servidor de Express esta en escucha  en el puerto 3000");
+ });
