4f77b6eac03994d6fde0ff93ac6798734a563292
joe-code-net <joe.code.netgmail.com>
2019-04-03 21:45:53 -0700

    MongoDB 2

--- db/index.js
+++ db/index.js
@@ -1,13 +1,30 @@
-const users = require("../resources/users");
+//const users = require("../resources/users");
+const mongo = require("./connect");
+const { DB_NAME } = require("./config");
 
 module.exports = {
     getUsers: function() {
+        const db = mongo.instance().db(DB_NAME);
+        const users = db.collection("users").find({}).toArray();
         return users;
     },
     getUserById: function(id) {
-        return users.filter(user=>user._id===id);
+        const db = mongo.instance().db(DB_NAME);
+        const users = db.collection("users").find({
+            _id: id
+        }).toArray();
+        //return users.filter(user=>user._id===id);
+        return users;
     },
     getUserByAgeRange: function(lower = 0, higher = 99) {
-        return users.filter(user=>user.age >= lower && user.age <= higher);
+        const db = mongo.instance().db(DB_NAME);
+        const users = db.collection("users").find({
+            age: {
+                $gte: Number(lower),
+                $lte: Number(higher)
+            }
+        }).toArray();
+        //return users.filter(user=>user.age >= lower && user.age <= higher);
+        return users;
     }
 }
