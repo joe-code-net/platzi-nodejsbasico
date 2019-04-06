f53a4eb5d2f4b0118c7d7405a78f47141748fa84
joe-code-net <joe.code.netgmail.com>
2019-04-06 15:24:43 -0700

    Tic tac toe 1

--- index.js
+++ index.js
@@ -5,3 +5,26 @@
  * ***************
  * 
  */
+const server = require ("http").Server();
+const io = require("socket.io")(server);
+const port = require("./config").SERVER_PORT;
+
+const banner = `
+****************************
+    Basic Node.js Course
+        Course Project
+     Tic Tac toe Server
+****************************
+Status: Online
+Listening on port: ${port}
+`;
+
+io.on("connection", (socket)=>{
+    socket.on("register", (user)=>{
+        console.info(`User registered: ${user.name}`)
+    })
+});
+
+server.listen(port, ()=>{
+    console.info(banner);
+});
