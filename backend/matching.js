64b90a3b95f46aea62ede5f26622e07ea815f147
joe-code-net <joe.code.netgmail.com>
2019-04-06 17:42:39 -0700

    Tic Tac Toe 3

--- backend/matching.js
+++ backend/matching.js
@@ -1,29 +1,54 @@
+const game = require("./gameState");
+
 module.exports = () => {
 	let players = {},
 		onWait = [],
 		onMatch = {};
 
+	/**
+	 * 
+	 * Comience aqui
+	 * 
+	 */
 	const loop = setInterval(checkQueue, 5000);
 
-	function printListStatus() {
-		console.info(`Queues:{ Players: ${Object.keys(players).length}, Onwait: ${onWait.length}; OnMatch: ${Object.keys(onMatch).length}; }`)
-	}
-
 	function checkQueue() {
-		printListStatus();
+		console.info(`Queues: { Players: ${Object.keys(players).length}, OnWait: ${onWait.length}, OnMatch: ${Object.keys(onMatch).length}}`)
+		//Imprima valores del pool
 		while (onWait.length >= 2) {
-			console.log("Constructing room...");
-			/**
-			 * 
-			 * Reemplazar este bloque
-			 * 
-			 */
-			const p1 = players[onWait.pop()].user.name;
-			const p2 = players[onWait.pop()].user.name;
-			console.log(`We created a match for ${p1} and ${p2}`)
+			console.log("Constructing room ... ")
+			// const p1 = players[onWait.pop()].user.name;
+			// const p2 = players[onWait.pop()].user.name;
+			// console.log(`We created a match for ${p1} and ${p2}`);
+			createMatch(onWait.pop(), onWait.pop())
 		}
 	}
 	
+	function createMatch(p1ID, p2ID) {
+		const roomId = p1ID + p2ID;
+		players[p1ID].roomId = roomId;
+		players[p2ID].roomId = roomId;
+
+		if (!onMatch[roomId]) 
+		onMatch[roomId] = game.newGame({
+			players: [players[p1ID], players[p2ID]],
+			roomId
+		});
+
+		players[p1ID].socket.emit("gameState", game.newGame({
+			players: [players[p1ID], players[p2ID]],
+			roomId,
+			playerId: 0,
+			opponentId: 1,
+		}));
+		players[p2ID].socket.emit("gameState", game.newGame({
+			players: [players[p1ID], players[p2ID]],
+			roomId,
+			playerId: 1,
+			opponentId: 0,
+		}));
+	}
+
 	return {
 		// user: {socket, user}
 		userConnect: ({ socket, user }) => {
